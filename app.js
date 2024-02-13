1c7f66edf80c1dbb71b21fd24209cba8988759b8
bnthurre <bushrahurre959@gmail.com>
2024-02-13 10:15:55 +0300

    undertanding how middlewares work

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const http = require('http');
-const express = require('express');
+const express = require("express");
 
-//This app object is the central piece that allows you to define routes, handle middleware, and manage the behavior of your web server. 
+//This app object is the central piece that allows you to define routes, handle middleware, and manage the behavior of your web server.
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    next();
-})
-app.use((req,res,next)=>{
-    console.log('in the another middleware')
-})
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("in the another middleware");
+  res.send("<h1>Heloo from expesss js</h1>");
+});
 
-const server = http.createServer(app)
-
-server.listen(8001);
-
+app.listen(8001);
+//app instance do work of creating server so i dont need createserver function
 
 //middleware (req,res,next) functions that handle requests and send appriciate resbond
