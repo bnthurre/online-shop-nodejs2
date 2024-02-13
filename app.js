747cbcd28375b5bb6f8697d3f652dae2da82af60
bnthurre <bushrahurre959@gmail.com>
2024-02-13 10:52:46 +0300

    parsing incoming request

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 //This app object is the central piece that allows you to define routes, handle middleware, and manage the behavior of your web server.
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/add-product',(req, res, next) => {
-  res.send("<h1>Heloo from add product page js</h1>");
+  res.send("<html><form action ='/product' method ='post'> <input type='text' name='title'><button type='submit'>submit</button></form></html>");
 });
+app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+  });
 app.use((req, res, next) => {
-  console.log("in the another middleware");
   res.send("<h1>Heloo from expesss js</h1>");
 });
 
@@ -18,3 +24,6 @@ app.listen(8001);
 //middleware (req,res,next) functions that handle requests and send appriciate resbond
 
 //Routes =url determine what happens when a user accesses a certain URL on your server.
+
+
+//body-parser package parses what enter requests but not files
