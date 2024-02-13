df8fc350bfb3c5d96002e85ab93b4bd76cb56b23
bnthurre <bushrahurre959@gmail.com>
2024-02-13 12:16:42 +0300

    using express routes

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 
-//This app object is the central piece that allows you to define routes, handle middleware, and manage the behavior of your web server.
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res, next) => {
-  res.send("<html><form action ='/product' method ='post'> <input type='text' name='title'><button type='submit'>submit</button></form></html>");
-});
-app.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-  });
-//this mean when form submited post request in this path do something
-app.use((req, res, next) => {
-  res.send("<h1>Heloo from expesss js</h1>");
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(8001);
+
+
+//This app object is the central piece that allows you to define routes, handle middleware, and manage the behavior of your web server.
+
 
 //app instance do work of creating server so i dont need createserver function
 
