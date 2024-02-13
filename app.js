2b5f4155333bd9ae990e75d6a5ff4a4d8f58d574
bnthurre <bushrahurre959@gmail.com>
2024-02-13 10:34:09 +0300

    adding diffrent routes

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require("express");
 //This app object is the central piece that allows you to define routes, handle middleware, and manage the behavior of your web server.
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use('/add-product',(req, res, next) => {
+  res.send("<h1>Heloo from add product page js</h1>");
 });
 app.use((req, res, next) => {
   console.log("in the another middleware");
@@ -13,6 +12,9 @@ app.use((req, res, next) => {
 });
 
 app.listen(8001);
+
 //app instance do work of creating server so i dont need createserver function
 
 //middleware (req,res,next) functions that handle requests and send appriciate resbond
+
+//Routes =url determine what happens when a user accesses a certain URL on your server.
