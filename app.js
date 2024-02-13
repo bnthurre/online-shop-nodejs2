5e1a8c0405a06f3e4a532cf879fcb5ee1fc11db4
bnthurre <bushrahurre959@gmail.com>
2024-02-13 10:09:51 +0300

    adding middlewares

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express');
 //This app object is the central piece that allows you to define routes, handle middleware, and manage the behavior of your web server. 
 const app = express();
 
+app.use((req,res,next)=>{
+    console.log('in the middleware')
+    next();
+})
+app.use((req,res,next)=>{
+    console.log('in the another middleware')
+})
+
 const server = http.createServer(app)
 
 server.listen(8001);
+
+
+//middleware (req,res,next) functions that handle requests and send appriciate resbond
