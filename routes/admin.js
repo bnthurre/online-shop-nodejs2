9d6d973961e54ce81dfead533e7681c10bf28fdc
bnthurre <bushrahurre959@gmail.com>
2024-02-14 08:52:34 +0300

    stylin pages

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const rootDir = require('../utils/rootDir');
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'admin.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
