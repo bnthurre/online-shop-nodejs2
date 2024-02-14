2dde9d1dab9e181639278d8ec7c95610292491ba
bnthurre <bushrahurre959@gmail.com>
2024-02-14 08:48:02 +0300

    using a helper function for navigation

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const rootDir = require('./utils/rootDir');
+
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop')
 const app = express();
@@ -12,7 +14,7 @@ app.use('/admin',adminRoute);
 app.use(shopRoute);
 
 app.use( (req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 app.listen(8001);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require('path');
 
+const rootDir = require('../utils/rootDir');
+
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'admin.html'))
+  res.sendFile(path.join(rootDir, 'views', 'admin.html'))
 });
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
