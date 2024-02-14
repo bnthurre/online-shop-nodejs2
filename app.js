403af82602ed49ac0a9055511eb0cf79911eab4a
bnthurre <bushrahurre959@gmail.com>
2024-02-14 12:01:24 +0300

    working with hbs

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
 const path = require('path');
 
 const rootDir = require('./utils/rootDir');
 
-const adminRoute = require('./routes/admin');
-const shopRoute = require('./routes/shop')
-
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', handlebars.engine());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+
+
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
@@ -24,9 +28,14 @@ app.use(shopRoute);
 // })
 
 //using pug 
+// app.use( (req,res,next)=>{
+//     res.status(404).render('404',{pageTitle: "page not found"});
+// })
+//using hbs
 app.use( (req,res,next)=>{
     res.status(404).render('404',{pageTitle: "page not found"});
 })
+
 app.listen(8001);
 
 
