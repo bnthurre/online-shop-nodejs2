6e926cc0408c31e06cd41b2742f158b753f37a41
bnthurre <bushrahurre959@gmail.com>
2024-02-15 10:41:54 +0300

    refoctoring file storage

--- models/productModel.js
+++ models/productModel.js
@@ -1,22 +1,25 @@
 const products = [];
 const fs = require("fs");
 const path = require("path");
-
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+getProductFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -24,16 +27,6 @@ module.exports = class Product {
     });
   }
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent)=>{
-        if(err){
-            cb([]);
-        }
-        cb(JSON.parse(fileContent));
-    })
+    getProductFromFile(cb);
   }
 };
