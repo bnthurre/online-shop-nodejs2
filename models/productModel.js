f40cba3195a0b0a06a999707b2de1a7cc8fc13eb
bnthurre <bushrahurre959@gmail.com>
2024-02-15 09:20:08 +0300

    storing data in files via model

--- models/productModel.js
+++ models/productModel.js
@@ -1,13 +1,39 @@
-const products =[];
+const { error, Console } = require("console");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
-    constructor(title){
-        this.title= title;
-    }
-    save(){
+  constructor(title) {
+    this.title = title;
+  }
+  save() {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products"
+    );
+    fs.readFile(p, (err, fileContent=>{
+        let products =[];
+        if(!err){
+            products= JSON.parse(fileContent);
+        }
         products.push(this);
-    }
-    static fetchAll(){
-        return products;
-    }
-}
+        fs.writeFile(p, JSON.stringify(products), err=>{
+            Console.log(err);
+        })   
+    }))
+  }
+  static fetchAll() {
+    const p = path.join(
+        path.dirname(require.main.filename),
+        "data",
+        "products"
+      );
+    fs.readFile(p,(err, fileContent)=>{
+        if(err){
+            return [];
+        }
+        return JSON.parse(fileContent);
+    })
+  }
+};
