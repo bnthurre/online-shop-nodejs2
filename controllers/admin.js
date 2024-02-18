e6c456c589ea7d71ebb15e584c167fccc44c6eae
bnthurre <bushrahurre959@gmail.com>
2024-02-18 09:34:29 +0300

    storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,16 +12,20 @@ exports.getAddProduct = (req, res, next) => {
 
 //get admin products
 exports.getProducts = (req, res, next) => {
-    res.render("admin/products", {
-      pageTitle: "admin products",
-      path: "/admin/products",
-    });
-  };
+  res.render("admin/products", {
+    pageTitle: "admin products",
+    path: "/admin/products",
+  });
+};
 
 //post add product
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title,imageUrl,price,description);
   product.save();
   res.redirect("/");
 };
--- models/productModel.js
+++ models/productModel.js
@@ -15,8 +15,11 @@ getProductFromFile = (cb) => {
   });
 };
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, price ,description) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.price =price;
+    this.description =description;
   }
   save() {
     getProductFromFile((products) => {
