70754f0e43be2d5bd3c7e03841a6932aaa45f953
bnthurre <bushrahurre959@gmail.com>
2024-02-28 15:06:07 +0300

    connecting db usin sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,20 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-guide',
-    password: 'admin'
-})
+//using sequalize
+const Sequelize = require("sequelize");
 
-module.exports = pool.promise();
+const sequelize = new Sequelize("node-guide", "root", "admin", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports =sequelize;
+
+//// using sql query
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-guide',
+//     password: 'admin'
+// })
+
+// module.exports = pool.promise();
