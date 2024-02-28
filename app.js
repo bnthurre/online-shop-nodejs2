faa4c4f36f4aa372f857e4dbfe796e98a19e4a40
bnthurre <bushrahurre959@gmail.com>
2024-02-28 13:25:25 +0300

    retreiving data from mysql db

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
 const path = require("path");
 
-
-const errorController = require('./controllers/error');
-const db = require('./utils/database');
+const errorController = require("./controllers/error");
+const db = require("./utils/database");
 
 const app = express();
 
@@ -23,7 +22,13 @@ app.set("views", "views");
 const adminRoute = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 
-db.execute('SELECT * FROM products').then().catch();
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
