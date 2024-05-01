010171b6859b27e10e030f4c4231a7e847638e8c
bnthurre <bushrahurre@gmail.com>
2024-04-30 22:55:10 -0700

    using magic associations method

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,12 +15,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  req.user.createProduct(
+    {
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      userId: req.user.id
+    }
+
+  )
     .then((result) => {
       console.log(result);
       res.redirect("/admin/products");
@@ -29,6 +33,27 @@ exports.postAddProduct = (req, res, next) => {
       console.log(err);
     });
 };
+// exports.postAddProduct = (req, res, next) => {
+//   const title = req.body.title;
+//   const imageUrl = req.body.imageUrl;
+//   const price = req.body.price;
+//   const description = req.body.description;
+
+//   Product.create({
+//     title: title,
+//     price: price,
+//     imageUrl: imageUrl,
+//     description: description,
+//     userId: req.user.id
+//   })
+//     .then((result) => {
+//       console.log(result);
+//       res.redirect("/admin/products");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
 
 //get edit product
 exports.getEditProduct = (req, res, next) => {
